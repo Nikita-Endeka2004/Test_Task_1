414ae59722ddc774d73c93c18a045c18e5caa26a
Nikta-Endeka2004 <endeka04062004@gmail.com>
2024-09-02 15:36:27 +0300

    Test github

--- app/src/router/router.tsx
+++ app/src/router/router.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/Layout";
-// import ErrorPage from "../pages/ErrorPage";
+import ErrorPage from "../pages/ErrorPage";
 import Home, { userLoader } from "../pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout/>,
-    // errorElement: <ErrorPage/>,
+    errorElement: <ErrorPage/>,
     children: [
       {
         index: true,
